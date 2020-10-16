7ee1fabcaab2a20882c0868d4bb2e26bf670f1df
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-16 02:20:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 numbers = [1, 2, 3]
 def square_array(numbers)
   numbers.each do | number |
-square = number ** 2
 numbers.shift
-puts "#{numbers}"
+square = number ** 2
 numbers.push square
 puts "#{numbers}"
 end
