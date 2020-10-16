56e471a9639953cdccc4a10cc54d0db349a546d8
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-16 02:06:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def new_numbers
+	[]
+end
+numbers = []
+def square_array(numbers)
+  numbers.each do | number |
+square = number ** 2
+new_numbers.push square
+end
 end
